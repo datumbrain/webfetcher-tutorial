47ead7cdcf204120005d7573ba180da9aba0b2a5
Faizan Khalid <faizan@datumbrain.com>
2020-02-20 15:58:49 +0500

    Initial Commit

---
+++ src/main/scala/com/datumbrain/webfetcher/MainClass.scala
@@ -0,0 +1,14 @@
+package com.datumbrain.webfetcher
+
+object MainClass {
+
+	def main(args : Array[String]): Unit = {
+
+		if (args.size != 1)
+			println("Invalid number of arguments..")
+
+		
+
+	}
+
+}
