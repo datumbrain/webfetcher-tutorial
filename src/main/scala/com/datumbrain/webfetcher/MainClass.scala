3268976a45512f96e3b68d92c79ff476ba3a1dd9
Faizan Khalid <faizan@datumbrain.com>
2020-02-21 15:14:10 +0500

    [WF-1] Basic Google Heading Fetcher
    Added basic functionality to work on with google search results page.
    
    This reads and stores headings from search results from google to a html file.

--- src/main/scala/com/datumbrain/webfetcher/MainClass.scala
+++ src/main/scala/com/datumbrain/webfetcher/MainClass.scala
@@ -7,8 +7,42 @@ object MainClass {
 		if (args.size != 1)
 			println("Invalid number of arguments..")
 
-		
+		import org.jsoup.Jsoup
+		import org.jsoup.nodes.Document
+		val doc: Document = Jsoup.connect(args(0)).get
+
+		//println(doc.title)
+		val titles = doc.getElementsByClass("LC20lb")
+
+		import java.io.File
+		import java.io.PrintWriter
+
+
+		val out_dir = new File("./output")
+		if (! out_dir.exists())
+			out_dir.mkdir();
+		val cur_dir = new File("./output/" + cleanURL(args(0)))
+		if (! cur_dir.exists())
+			cur_dir.mkdir();
+		val htmlFile = new File("output/" + cleanURL(args(0)) + "/output.html")
+		val htmlWriter = new PrintWriter(htmlFile)
+		import scala.collection.JavaConversions._
+		for (title <- titles) {
+			htmlWriter.write(title.text() + "<br />\n")
+		}
+		htmlWriter.close()
+
+		val urlFile = new File("output/" + cleanURL(args(0)) + "/output.url")
+		val urlWriter = new PrintWriter(urlFile)
+		urlWriter.write("[InternetShortcut]\n")
+		urlWriter.write("URL="+ args(0) +"\n")
+		urlWriter.close()
 
 	}
 
+
+	def cleanURL(url: String):String = {
+		val s = url.replaceAll("[^a-zA-Z0-9 ]", "")
+		s
+	};
 }
