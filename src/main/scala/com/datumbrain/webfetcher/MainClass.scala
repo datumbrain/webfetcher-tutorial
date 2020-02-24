866d84cb5bccd69749004f4da0faa75081cfac10
Faizan Khalid <faizan@datumbrain.com>
2020-02-24 18:27:40 +0500

    [WF-2] HTML Tag Fetcher
    Shows headings and their count
    - h1
    - h2
    - h3
    
    Shows links and their count
    - Inbound links
    - Outbound links
    
    Shows images count and their source links.
    
    Better output format.

--- src/main/scala/com/datumbrain/webfetcher/MainClass.scala
+++ src/main/scala/com/datumbrain/webfetcher/MainClass.scala
@@ -1,48 +1,140 @@
 package com.datumbrain.webfetcher
+import java.io.File
+import java.io.PrintWriter
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import scala.collection.JavaConversions._
 
 object MainClass {
 
-	def main(args : Array[String]): Unit = {
+	def main(args: Array[String]): Unit = {
 
 		if (args.size != 1)
 			println("Invalid number of arguments..")
 
-		import org.jsoup.Jsoup
-		import org.jsoup.nodes.Document
-		val doc: Document = Jsoup.connect(args(0)).get
-
-		//println(doc.title)
-		val titles = doc.getElementsByClass("LC20lb")
-
-		import java.io.File
-		import java.io.PrintWriter
+		try {
+			//Use Jsoup to connect
+			println("Connecting to `"+args(0)+"`..")
+			val doc: Document = Jsoup.connect(args(0)).get
 
 
-		val out_dir = new File("./output")
-		if (! out_dir.exists())
-			out_dir.mkdir();
-		val cur_dir = new File("./output/" + cleanURL(args(0)))
-		if (! cur_dir.exists())
-			cur_dir.mkdir();
-		val htmlFile = new File("output/" + cleanURL(args(0)) + "/output.html")
-		val htmlWriter = new PrintWriter(htmlFile)
-		import scala.collection.JavaConversions._
-		for (title <- titles) {
-			htmlWriter.write(title.text() + "<br />\n")
+			// Setup directory
+			println("Checking output path..")
+			val out_dir = new File("./output")
+			if (!out_dir.exists())
+				out_dir.mkdir();
+			val cur_dir = new File("./output/" + cleanURL(args(0)))
+			if (!cur_dir.exists())
+				cur_dir.mkdir();
+
+
+			// Write .url file
+			println("Creating output.url..")
+			val urlFile = new File("output/" + cleanURL(args(0)) + "/output.url")
+			val urlWriter = new PrintWriter(urlFile)
+			//urlWriter.write("[InternetShortcut]\n")
+			//urlWriter.write("URL="+ args(0) +"\n")
+			urlWriter.write(args(0))
+			urlWriter.close()
+
+
+			// Write .html file
+			println("Creating output.html..")
+			val htmlFile = new File("output/" + cleanURL(args(0)) + "/output.html")
+			val htmlWriter = new PrintWriter(htmlFile)
+
+
+			htmlWriter.write(
+				s"""<html>
+				  |<head>
+				  |	<title>Web Fetcher Results</title>
+				  |</head>
+				  |<body>
+				  |	<h1>Datum Brain — Web Fetcher</h1>
+				  | <b>${doc.title()}</b><br/>
+				  |	<small>${args(0)}</small><br/>
+				  | <br/>""".stripMargin)
+
+			// Headings
+			println("Parsing headings..")
+			printElementsTextWithTag(doc,"h1",htmlWriter)
+			printElementsTextWithTag(doc,"h2",htmlWriter)
+			printElementsTextWithTag(doc,"h3",htmlWriter)
+
+
+			// Links
+			println("Parsing links..")
+			val a = doc.select("a")
+			htmlWriter.write("<h3>Links (" + a.size + ")</h3><ul>")
+			val domain = getDomain(args(0))
+
+			def inboundLinks = for {
+				link <- a
+				if (hasDomain(link.attr("href")) && getDomain(link.attr("href")) == domain)
+			} yield link
+			def outboundLinks = for {
+				link <- a
+				if (hasDomain(link.attr("href")) && getDomain(link.attr("href")) != domain)
+			} yield link
+
+			println("Parsing inbound links..")
+			htmlWriter.write("<h3>Inbound (" + inboundLinks.size + ")</h3><ul>")
+			for (e <- inboundLinks) htmlWriter.write("<li><b>" + e.text() + "</b> ( <u><i>" + e.attr("href") + "</i></u> )</li>\n")
+			htmlWriter.write("</ul>")
+
+			println("Parsing outbound links..")
+			htmlWriter.write("<h3>Outbound (" + outboundLinks.size + ")</h3><ul>")
+			for (e <- outboundLinks) htmlWriter.write("<li><b>" + e.text() + "</b> ( <u><i>" + e.attr("href") + "</i></u> )</li>\n")
+			htmlWriter.write("</ul>")
+
+
+
+			// Images
+			println("Parsing images..")
+			val img = doc.select("img")
+			htmlWriter.write("<h3>Images (" + img.size + ")</h3><ul>")
+			for (e <- img) htmlWriter.write("<li>" + e.attr("src") + "</li>\n")
+			htmlWriter.write("</ul>")
+
+
+			htmlWriter.write("</body></html>")
+
+			htmlWriter.close()
+
+			println("All done..!")
+
+		} catch {
+			case ex: java.net.UnknownHostException => {
+				println("Error: cannot connect to `"+args(0)+"`")
+				System.exit(1)
+			}
+			case ex: javax.net.ssl.SSLException => {
+				println("Error: cannot connect to `"+args(0)+"`")
+				System.exit(1)
+			}
+			case ex: javax.net.ssl.SSLHandshakeException => {
+				println("Error: connection to `"+args(0)+"` was reset..")
+				System.exit(1)
+			}
+
 		}
-		htmlWriter.close()
-
-		val urlFile = new File("output/" + cleanURL(args(0)) + "/output.url")
-		val urlWriter = new PrintWriter(urlFile)
-		urlWriter.write("[InternetShortcut]\n")
-		urlWriter.write("URL="+ args(0) +"\n")
-		urlWriter.close()
 
 	}
 
+	def hasDomain(url: String) = (url.split("//").length == 2)
 
-	def cleanURL(url: String):String = {
-		val s = url.replaceAll("[^a-zA-Z0-9 ]", "")
-		s
-	};
+	def getDomain(url: String) = url.split("//")(1).split("/")(0)
+
+	def cleanURL(url: String) = url.replaceAll("[^a-zA-Z0-9 ]", "")
+
+	def printElementsTextWithTag(doc: org.jsoup.nodes.Document, tag: String, file: PrintWriter) =  {
+
+		val arr = doc.select(tag)
+
+		file.write("<h3>"+tag+" (" + arr.size + ")</h3><ul>")
+		for (e <- arr) file.write("<li>" + e.text() + "</li>\n")
+		file.write("</ul>")
+	}
+
 }
